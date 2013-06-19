5c08b3d2a0e8f8892f96502641cd72e5cc107a18
Juan Eyzaguirre <jieyzaguirre@gmail.com>
2013-06-19 14:56:03 -0400

    Created web app for debate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debate::Application.config.secret_token = 'cf0df4ec3101b39c64fcea5bff4da0613fc9b1b89645e3e7a895d2a5727bd262caa46ce9eb077351fa328ce96d5b026bdc7e9a6181660adc1163e4576c5b1ab9'
